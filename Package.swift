7238085149a31dd3bc4f16b0b0410f3aa26539ce
atlas <67726869+rs-atlas@users.noreply.github.com>
2023-04-03 14:32:04 +0900

    update package.swift

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
 // swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+//    platforms: [.iOS(.v9), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2)],
 
 import PackageDescription
 
 let package = Package(
     name: "UtilityKit",
-    platforms: [.iOS(.v13), .macOS(.v13), .tvOS(.v13), .watchOS(.v6)],
+    platforms: [.iOS(.v11), .macOS(.v10_13), .tvOS(.v11), .watchOS(.v4)],
     products: [
-        
         .library(
             name: "UtilityKit",
             targets: ["UtilityKit"]),
@@ -22,3 +22,4 @@ let package = Package(
     ],
     swiftLanguageVersions: [.v5 ]
 )
+//    platforms: [.iOS(.v9), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v2)],
